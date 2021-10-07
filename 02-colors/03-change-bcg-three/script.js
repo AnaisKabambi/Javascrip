771acb3e14005637bad1b200b49dcbb9a80645ec
Anais Kabambi <anais.k@hotmail.be>
2021-10-07 12:14:34 +0200

    revisited 02

--- 02-colors/03-change-bcg-three/script.js
+++ 02-colors/03-change-bcg-three/script.js
@@ -9,6 +9,8 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
+
 (function() {
     let randomColor = "#" + ((1<<24)*Math.random() | 0).toString(16);
     document.getElementById("body").style.backgroundColor = randomColor;
