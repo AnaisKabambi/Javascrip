e19ec78b451fcb0057f65a9f59e63e366ae45f42
Anais Kabambi <anais.k@hotmail.be>
2021-10-07 11:43:02 +0200

    revisited 01

--- 01-base/04-agt-confirm/script.js
+++ 01-base/04-agt-confirm/script.js
@@ -9,8 +9,35 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function(questions) {
 
     // your code here
+    let age = prompt("What is your age?");
+    let gender = prompt("What is your gender?");
+    let town = prompt("Where do you live?");
+    console.log(questions);
 
-})();
+    /*function myFunction() {
+        var txt;
+        if (confirm("You are" + age + "years old," + gender + "and you live in" + town + ". Is that correct?")) {
+            txt = "Great!";
+        } else {
+            txt = "Let's try again";
+        }
+        console.log (myFunction())
+    }*/
+    (function myFunction() {
+        var retVal = confirm("You are" + age + "years old," + gender + "and you live in" + town + ". Is that correct?");
+        if (retVal == true) {
+            document.write("Great!");
+            return true;
+        } else {
+            document.write("Let's try again");
+            questions();
+            return false;
+        }
+        ;
+        console.log(myFunction());
+    })
+    ();
+})
